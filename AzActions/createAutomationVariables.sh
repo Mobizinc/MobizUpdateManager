e6740982cc1780b9f153bbcec2da568cce3e45b2
RoshanMarcus <132414976+RoshanMarcus@users.noreply.github.com>
2023-05-26 11:50:01 +0530

    Update createAutomationVariables.sh

--- AzActions/createAutomationVariables.sh
+++ AzActions/createAutomationVariables.sh
@@ -1,2 +1,3 @@
 az config set extension.use_dynamic_install=yes_without_prompt
+az automation variable create --name 'UpdateMgrAutomationAccountName' --value 'Need to Add' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
 
