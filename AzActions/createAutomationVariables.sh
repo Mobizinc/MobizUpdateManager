c813cf7f617ae6b02acc156382793c6f0535a9b1
RoshanMarcus <132414976+RoshanMarcus@users.noreply.github.com>
2023-05-26 11:35:27 +0530

    Update createAutomationVariables.sh

--- AzActions/createAutomationVariables.sh
+++ AzActions/createAutomationVariables.sh
@@ -1,6 +1,6 @@
 az config set extension.use_dynamic_install=yes_without_prompt
-az automation variable create --name 'UpdateMgrAutomationAccountName' --value 'Need To Add' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
-az automation variable create --name 'UpdateMgrRG' --value 'Need To Add' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
-az automation variable create --name 'UpdateMgrScheduleMonitorQueue' --value 'schedulemonitorqueue' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
-az automation variable create --name 'UpdateMgrStorageAccount' --value 'Need To Add' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
-az automation variable create --name 'UpdateMgrSummaryQueue' --value 'updatesummary' --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
+az automation variable create --name "UpdateMgrAutomationAccountName" --value "Need To Add" --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
+az automation variable create --name "UpdateMgrRG" --value "Need To Add" --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
+az automation variable create --name "UpdateMgrScheduleMonitorQueue" --value "schedulemonitorqueue" --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
+az automation variable create --name "UpdateMgrStorageAccount" --value "Need To Add" --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
+az automation variable create --name "UpdateMgrSummaryQueue" --value "updatesummary" --automation-account-name $AutomationAccountName --resource-group $ResourceGroupName
