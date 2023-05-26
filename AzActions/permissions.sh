32449a6db9237f22cb80462dd85fa1c1abc9d83d
Georgejoy7 <79727462+Georgejoy7@users.noreply.github.com>
2023-05-26 09:51:41 -0400

    Create permissions.sh

---
+++ AzActions/permissions.sh
@@ -0,0 +1,45 @@
+
+role_name="Storage Queue Data Contributor"
+# Get the Automation Account resource ID
+automation_account_id=$(az automation account show --name "$AutomationAccountName" --query "id" --output tsv)
+
+# Get the Storage Account resource ID
+storage_account_id=$(az storage account show --name "$StorageAccountName" --query "id" --output tsv)
+
+# Check if the role assignment already exists
+existing_role_assignment=$(az role assignment list --assignee "$automation_account_id" --scope "$storage_account_id" --output tsv)
+
+# Create the role assignment if it doesn't exist
+if [[ -z "$existing_role_assignment" ]]; then
+    az role assignment create --assignee "$automation_account_id" --scope "$storage_account_id" --role "$role_name"
+    echo "Role assignment created. $role_name"
+else
+    echo "Role assignment already exists. $role_name"
+fi
+
+role_name="Storage Account Key Operator Service Role"
+existing_role_assignment=$(az role assignment list --assignee "$automation_account_id" --scope "$storage_account_id" --output tsv)
+
+# Create the role assignment if it doesn't exist
+if [[ -z "$existing_role_assignment" ]]; then
+    az role assignment create --assignee "$automation_account_id" --scope "$storage_account_id" --role "$role_name"
+    echo "Role assignment created. $role_name"
+else
+    echo "Role assignment already exists. $role_name"
+fi
+#logicapp
+role_name="Storage Queue Data Contributor"
+logicapp_id=$(az logicapp show --name "$LogicAppName" --query "id" --output tsv) 
+# Get the Storage Account resource ID
+storage_account_id=$(az storage account show --name "$StorageAccountName" --query "id" --output tsv)
+
+# Check if the role assignment already exists
+existing_role_assignment=$(az role assignment list --assignee "$logicapp_id" --scope "$storage_account_id" --output tsv)
+
+# Create the role assignment if it doesn't exist
+if [[ -z "$existing_role_assignment" ]]; then
+    az role assignment create --assignee "$logicapp_id" --scope "$storage_account_id" --role "$role_name"
+    echo "Role assignment created. $role_name"
+else
+    echo "Role assignment already exists. $role_name"
+fi
