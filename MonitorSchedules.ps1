ce799fca6876e353c7164c37f4ad1a55c0b9e36b
RoshanMarcus <132414976+RoshanMarcus@users.noreply.github.com>
2023-05-26 09:18:09 +0530

    Update MonitorSchedules.ps1

--- MonitorSchedules.ps1
+++ MonitorSchedules.ps1
@@ -1,168 +1,75 @@
-<#PSScriptInfo
-.VERSION 1.1
-.GUID 9606f2a1-49f8-4a67-91d6-23fc6ebf5b3b
-.AUTHOR zachal
-.COMPANYNAME Microsoft 
-.COPYRIGHT 
-.TAGS UpdateManagement, Automation
-.LICENSEURI 
-.PROJECTURI 
-.ICONURI 
-.EXTERNALMODULEDEPENDENCIES ThreadJob
-.REQUIREDSCRIPTS 
-.EXTERNALSCRIPTDEPENDENCIES 
-.RELEASENOTES
-Removed parameters AutomationAccount, ResourceGroup
-Changed run as to managed identity
-.PRIVATEDATA 
-#>
-<# 
-.DESCRIPTION 
-This script is intended to be run as a part of Update Management Pre/Post scripts.
-It requires a RunAs account.
-This script will ensure all Azure VMs in the Update Deployment are running so they recieve updates.
-This script works with the Turn Off VMs script. It will store the names of machines that were started in an Automation variable so only those machines are turned back off when the deployment is finished.
-#> 
-#requires -Modules ThreadJob
-<#
-.SYNOPSIS
- Stop VMs that were started as part of an Update Management deployment
-.DESCRIPTION
-  This script is intended to be run as a part of Update Management Pre/Post scripts. 
-  It requires a RunAs account.
-  This script will turn off all Azure VMs that were started as part of TurnOnVMs.ps1.
-  It retrieves the list of VMs that were started from an Automation Account variable.
-.PARAMETER SoftwareUpdateConfigurationRunContext
-  This is a system variable which is automatically passed in by Update Management during a deployment.
-#>
+name: Deploy Resources
 
-param(
-    [string]$SoftwareUpdateConfigurationRunContext
-)
+on:
+  workflow_dispatch:
 
-#region BoilerplateAuthentication
-#This requires a RunAs account
-# $ServicePrincipalConnection = Get-AutomationConnection -Name 'AzureRunAsConnection'
+env:
 
-# Add-AzAccount `
-#     -ServicePrincipal `
-#     -TenantId $ServicePrincipalConnection.TenantId `
-#     -ApplicationId $ServicePrincipalConnection.ApplicationId `
-#     -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint
+  LOGICAPP_DIRECTORY: LogicApp
 
-$AzureContext = (Connect-AzAccount -Identity ).context
-$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+jobs:
+  deploy-and-run:
+    runs-on: ubuntu-latest
+    environment: qa
+    steps:
+       - name: Checkout repository
+         uses: actions/checkout@v2
+        
+       - name: Create Azure cred variable
+         run: |
+          AzureCreds=$(echo '{"clientId": "${{secrets.AZURE_CLIENT_ID}}", "clientSecret": "${{secrets.AZURE_CLIENT_SECRET}}", "subscriptionId": "${{secrets.AZURE_SUBSCRIPTION_ID}}", "tenantId": "${{secrets.AZURE_TENANT_ID}}"}' | jq -c .)
+          echo "AzureCreds=$AzureCreds" >> $GITHUB_ENV
+          logicAppName=$(jq -r '.parameters.LogicAppName.value' parameters.json)
+          echo "LogicAppName=$logicAppName" >> $GITHUB_ENV
+          automationAccountName=$(jq -r '.parameters.AutomationAccountName.value' parameters.json)
+          echo "AutomationAccountName=$automationAccountName" >> $GITHUB_ENV
 
-#If you wish to use the run context, it must be converted from JSON
-$context = ConvertFrom-Json  $SoftwareUpdateConfigurationRunContext
-$runId = "PrescriptContext" + $context.SoftwareUpdateConfigurationRunId
+       - uses: azure/login@v1
+         with:
+          creds: ${{ env.AzureCreds }}
 
-Write-Output "SoftwareUpdateConfigurationRunId : $($runId)"
-#Retrieve the automation variable, which we named using the runID from our run context. 
-#See: https://docs.microsoft.com/en-us/azure/automation/automation-variables#activities
-$variable = Get-AutomationVariable -Name $runId
-if (!$variable) 
-{
-    Write-Output "No machines to turn off"
-    return
-}
-
-#https://github.com/azureautomation/runbooks/blob/master/Utility/ARM/Find-WhoAmI
-# In order to prevent asking for an Automation Account name and the resource group of that AA,
-# search through all the automation accounts in the subscription 
-# to find the one with a job which matches our job ID
-$AutomationResource = Get-AzResource -ResourceType Microsoft.Automation/AutomationAccounts
-
-foreach ($Automation in $AutomationResource)
-{
-    $Job = Get-AzAutomationJob -ResourceGroupName $Automation.ResourceGroupName -AutomationAccountName $Automation.Name -Id $PSPrivateMetadata.JobId.Guid -ErrorAction SilentlyContinue
-    if (!([string]::IsNullOrEmpty($Job)))
-    {
-        $ResourceGroup = $Job.ResourceGroupName
-        $AutomationAccount = $Job.AutomationAccountName
-        break;
-    }
-}
-
-$vmIds = $variable -split ","
-$stoppableStates = "starting", "running"
-$jobIDs= New-Object System.Collections.Generic.List[System.Object]
-
-#This script can run across subscriptions, so we need unique identifiers for each VMs
-#Azure VMs are expressed by:
-# subscription/$subscriptionID/resourcegroups/$resourceGroup/providers/microsoft.compute/virtualmachines/$name
-$vmIds | ForEach-Object {
-    $vmId =  $_
     
-    $split = $vmId -split "/";
-    $subscriptionId = $split[2]; 
-    $rg = $split[4];
-    $name = $split[8];
-    Write-Output ("Subscription Id: " + $subscriptionId)
-    $mute = Select-AzSubscription -Subscription $subscriptionId
 
-    $vm = Get-AzVM -ResourceGroupName $rg -Name $name -Status -DefaultProfile $mute
+       - name: Deploy ARM Template
+         uses: azure/arm-deploy@v1
+         with:
+          resourceGroupName: ${{ vars.RESOURCE_GROUP_NAME }}
+          template: deploy.json
+          parameters: parameters.json 
+         
+         
 
-    $state = ($vm.Statuses[1].DisplayStatus -split " ")[1]
-    if($state -in $stoppableStates) {
-        Write-Output "Stopping '$($name)' ..."
-		$vmContext = Select-AzSubscription -Subscription $subscriptionId		
-        $newJob= Stop-AzVM -ResourceGroupName $rg -Name $name -Force -DefaultProfile $vmContext -AsJob 
-        #$newJob = Start-ThreadJob -ScriptBlock { param($resource, $vmname, $sub) $context = Select-AzSubscription -Subscription $sub; Stop-AzVM -ResourceGroupName $resource -Name $vmname -Force -DefaultProfile $context} -ArgumentList $rg,$name,$subscriptionId
-        $jobIDs.Add($newJob.Id)
-    }else {
-        Write-Output ($name + ": already stopped. State: " + $state) 
-    }
-}
-#Wait for all machines to finish stopping so we can include the results as part of the Update Deployment
-$jobsList = $jobIDs.ToArray()
-if ($jobsList)
-{
-    Write-Output "Waiting for machines to finish stopping..."
-    Wait-Job -Id $jobsList
-}
+       - name: Zip logicapp files
+         run: (cd ${{ env.LOGICAPP_DIRECTORY }} && zip -rq ../${{ github.run_id }}.zip ./* -x /*workflow-designtime/*)
+       - name: Upload Zip logicapp files
+         uses: actions/upload-artifact@master
+         with:
+          name: build-artifact
+          path: ${{ github.run_id }}.zip
 
-foreach($id in $jobsList)
-{
-    $job = Get-Job -Id $id
-    if ($job.Error)
-    {
-        Write-Output $job.Error
-    }
-}
-#Clean up our variables:
-Remove-AzAutomationVariable -AutomationAccountName $AutomationAccount -ResourceGroupName $ResourceGroup -name $runID
+       - name: Download logicapp files
+         uses: actions/download-artifact@master
+         with:
+           name: build-artifact
+           path: build-art/
+
+       - name: Deploy to Azure Logic App
+         uses: Azure/functions-action@v1
+         id: la
+         with:
+          app-name: ${{ env.LogicAppName }}
+          package: build-art/${{ github.run_id }}.zip
+          resource-group: ${{ vars.RESOURCE_GROUP_NAME }}
+          credentials: ${{ secrets.AZURE_CREDENTIALS }}
+       - name: Update Runbook Content
+         run: |
+          ls
+          az config set extension.use_dynamic_install=yes_without_prompt
+          az automation runbook replace-content --automation-account-name "${{ env.AutomationAccountName }}" --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --name "TurnOnVMs-UpdateStart" --content @TurnOnVMs-UpdateStart.ps1
+          az automation runbook publish --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --automation-account-name "${{ env.AutomationAccountName }}" --name "TurnOnVMs-UpdateStart"
+          az automation runbook replace-content --automation-account-name "${{ env.AutomationAccountName }}" --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --name "TurnOffVMs-UpdateEnds" --content @TurnOffVMs-UpdateEnds.ps1
+          az automation runbook publish --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --automation-account-name "${{ env.AutomationAccountName }}" --name "TurnOffVMs-UpdateEnds"
+          az automation runbook replace-content --automation-account-name "${{ env.AutomationAccountName }}" --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --name "MonitorSchedules" --content @MonitorSchedules.ps1
+          az automation runbook publish --resource-group ${{ vars.RESOURCE_GROUP_NAME }} --automation-account-name "${{ env.AutomationAccountName }}" --name "MonitorSchedules"
 
 
-
-#UpdateManagement
-
-#Update Management Additional Tasks
-$context = ConvertFrom-Json  $SoftwareUpdateConfigurationRunContext
-$vmIds = $context.SoftwareUpdateConfigurationSettings.AzureVirtualMachines
-#Get Storage details
-$storageRGName=Get-AutomationVariable -Name "UpdateMgrRG"
-$storageAccountName=Get-AutomationVariable -Name "UpdateMgrStorageAccount"
-$storageQueueName=Get-AutomationVariable -Name "UpdateMgrSummaryQueue"
-
-#Set Context
-$AzureContext = (Connect-AzAccount -Identity ).context
-$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
-
-#Get Key and set Storage Context
-$key = (Get-AzStorageAccountKey -ResourceGroupName $storageRGName -Name $storageAccountName)[0].Value
-
-$updateMgrStoragecontext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key
-
-$queue = Get-AzStorageQueue –Name $storageQueueName –Context $updateMgrStoragecontext
-
-$updateInfo=@{
-    runId=$context.SoftwareUpdateConfigurationRunId
-    virtualMachines=$context.SoftwareUpdateConfigurationSettings.AzureVirtualMachines
-}
-
-$updateInfo =$updateInfo | ConvertTo-Json
-
-$queueMessage = [Microsoft.Azure.Storage.Queue.CloudQueueMessage]::new($updateInfo)
-
-$queue.CloudQueue.AddMessageAsync($queueMessage)
