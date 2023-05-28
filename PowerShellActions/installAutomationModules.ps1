a99331a7b98050c4bd5309f6360138fd2f2a5864
RoshanMarcus <roshanmarcus22@gmail.com>
2023-05-28 23:44:55 +0530

    Module and Schedule Management

---
+++ PowerShellActions/installAutomationModules.ps1
@@ -0,0 +1,2 @@
+#Install AZTable Module
+New-AzAutomationModule -AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName -Name 'AzTable' -ContentLinkUri "https://www.powershellgallery.com/api/v2/package/AzTable/2.1.0"
--- PowerShellActions/setUpAutomationSchedules.ps1
+++ PowerShellActions/setUpAutomationSchedules.ps1
@@ -1,1 +1,9 @@
- Get-AzResourceGroup
+$StartTime = (Get-Date "13:00:00").AddDays(1)
+New-AzAutomationSchedule -AutomationAccountName $AutomationAccountName -Name "MonitorScheduleHourly" -StartTime $StartTime -HourInterval 1 -ResourceGroupName $ResourceGroupName
+
+$runbookName = "MonitorSchedules"
+$scheduleName = "MonitorScheduleHourly"
+
+Register-AzAutomationScheduledRunbook -AutomationAccountName $AutomationAccountName `
+-Name $runbookName -ScheduleName $scheduleName `
+-ResourceGroupName $ResourceGroupName
